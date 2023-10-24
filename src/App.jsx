c744eafd44436ed52e83c2026e16bcf1ff2a46fb
Nikhil Prasad <nprasad2077@gmail.com>
2023-10-24 06:52:08 -0500

    stateless

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import FormWithoutState from "./components/FormWithoutState";
 
 function App() {
 
   return (
     <div>
       <h1>Hello this is a react-forms demo</h1>
+      <FormWithoutState />
     </div>
   );
 }
--- src/components/FormWithoutState.jsx
+++ src/components/FormWithoutState.jsx
@@ -3,16 +3,42 @@ import { useEffect, useRef } from "react";
 
 const FormWithoutState = () => {
   const countRef = useRef(0);
-  
+
   useEffect(() => {
-    countRef.current = countRef.current + 1
-  })
+    countRef.current = countRef.current + 1;
+  });
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-  }
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+    const email = form.get("email");
+    const password = form.get("password");
+    console.log({ email, password });
 
-  return <div>FormWithoutState</div>;
+    const body = {};
+
+    for (const [key, value] of form.entries()) {
+      body[key] = value;
+    }
+
+    console.log(body);
+  };
+
+  return (
+    <div>
+      <h2>Form without State</h2>
+      <h6>Current renders {countRef.current}</h6>
+      <form onSubmit={handleSubmit}>
+        <div className="input-field">
+          <input type="email" name="email"></input>
+        </div>
+        <div className="input-field">
+          <input type="password" name="password"></input>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default FormWithoutState;
